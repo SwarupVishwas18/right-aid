9f03aa43a7b470600ff03470872db0dd262f739e
Swarup Vishwas <swarupvishwas000@gmail.com>
2023-09-23 19:25:06 +0530

    Decline case

--- right_aid/lib/main.dart
+++ right_aid/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const MyHomePage(title: 'Right Aid'),
-      home: const SplashScreen(),
+      home: const CurrentCase(),
     );
   }
 }
